f93b8ca50283a764852dd85205b5116bd66c993e
PL <philip.lundin@iths.se>
2021-03-23 17:08:29 +0100

    Step 1

--- src/main/java/Calculator.java
+++ src/main/java/Calculator.java
@@ -1,8 +1,17 @@
 public class Calculator {
 
-    int add(String numbers){
+    int add(String numbers) {
+       String[] listOfNumbers = numbers.split(", ");
 
-        return 0;
+        if (isEmpty(numbers)){
+            return 0;
+        }
+        else {
+            return Integer.parseInt(listOfNumbers[0]) + Integer.parseInt(listOfNumbers[1]);
+        }
     }
 
+    private boolean isEmpty(String numbers){
+        return numbers.isEmpty();
+    }
 }
--- src/test/java/CalculatorTest.java
+++ src/test/java/CalculatorTest.java
@@ -1,6 +1,31 @@
-public class CalculatorTest {
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.Assertions;
 
-    Calculator calc = new Calculator();
+import static org.junit.jupiter.api.Assertions.*;
 
 
+class CalculatorTest {
+
+    private Calculator calculator;
+
+    @BeforeEach
+    public void beforeEachTest() {
+        calculator = new Calculator();
+    }
+
+    @Test
+    void emptyString() {
+        assertEquals(calculator.add(""), 0);
+    }
+
+    @Test
+    void singleNumber() {
+        assertEquals(calculator.add("1"), 1);
+    }
+
+    @Test
+    void twoNumbers() {
+        assertEquals(calculator.add("1, 2"), 3);
+    }
 }
